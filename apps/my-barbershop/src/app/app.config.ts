741b05f1c76c5f8a7a05c213f66a33b6c7c5e2b4
Guilherme <gferreiradasilva96@gmail.com>
2025-03-26 21:48:22 -0300

    ✨ feat: Transloco and arch pastes

--- apps/my-barbershop/src/app/app.config.ts
+++ apps/my-barbershop/src/app/app.config.ts
@@ -1,10 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { provideHttpClient } from '@angular/common/http';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: ['pt-BR', 'en'],
+        defaultLang: 'pt-BR',
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
   ],
 };
